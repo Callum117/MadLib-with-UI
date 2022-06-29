e1537b4b27dbadbe6b33396ed2c3584043995e37
Callum117 <yoda@evil.geek.nz>
2022-06-30 08:08:55 +1200

    add program

---
+++ main/main.py
@@ -0,0 +1,7 @@
+
+def concatenate(nameOfPerson):
+    stringToReturn = f"Enchante, {nameOfPerson}."
+    return stringToReturn
+
+nameOfPerson = input("What is your name? \n>>>")
+print(concatenate(nameOfPerson))
